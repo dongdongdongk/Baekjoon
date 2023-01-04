380d2e840b9039dac58a97a4e8a025a7501252c4
dongdongdongk <dhk9309@gmail.com>
2023-01-05 02:15:30 +0900

    test

---
+++ src/summin/B18108.java
@@ -0,0 +1,14 @@
+package summin;
+
+import java.util.Scanner;
+
+public class B18108 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int bg = sc.nextInt();
+		int ad = bg - 543;
+		System.out.println(ad);
+		
+	}
+}
---
+++ src/summin/B3003.java
@@ -0,0 +1,14 @@
+package summin;
+
+import java.util.Scanner;
+
+public class B3003 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int [] all = new int[6];
+		for(int i=0;i<6;i++) {
+			all[i] = sc.nextInt();
+		}
+		
+	}
+}
